03b7a8cc5a053416e5c9115c0ed506d5d705110b
David <Davidteri91@gmail.com>
2017-01-14 20:06:16 +0100

    display: fix QCOM_BSP build
    remove display-frameworks dependency
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -26,6 +26,9 @@
 
 #include <fcntl.h>
 #include <math.h>
+#ifdef QTI_BSP
+#include <hardware/display_defs.h>
+#endif
 #include <hardware/hwcomposer.h>
 #include <gr.h>
 #include <gralloc_priv.h>
@@ -36,7 +39,6 @@
 #include <overlayRotator.h>
 #include <EGL/egl.h>
 #include <QService.h>
-
 
 #define ALIGN_TO(x, align)     (((x) + ((align)-1)) & ~((align)-1))
 #define LIKELY( exp )       (__builtin_expect( (exp) != 0, true  ))
@@ -49,10 +51,6 @@
 #define STR(f) #f;
 // Max number of PTOR layers handled
 #define MAX_PTOR_LAYERS 2
-
-#ifdef QTI_BSP
-#include <exhwcomposer_defs.h>
-#endif
 
 //Fwrd decls
 struct hwc_context_t;
